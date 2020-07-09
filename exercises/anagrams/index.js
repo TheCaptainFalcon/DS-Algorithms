6c1560bffaaa8c4392deaff1139b2df0046ef4b2
Joseph <josephkim570@gmail.com>
2020-07-09 19:24:43 -0400

    added solution for anagrams using a helper function with regex, split, sort, and join.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,15 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+   return cleanString(stringA) === cleanString(stringB)
+}
+
+// regex \w matches any non-word character [^a-zA-z0-9_] aka only letters and numbers, it also removes underscore vs \w_ which will negate the _. 
+// the second set of '' after the regex - replaces all non word characters into a space
+// lowercase as instructions provided, split with '' turns into a sublist array, separated by commas, sort is alphabetical, join removes the commas with ''
+function cleanString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
 
 module.exports = anagrams;
