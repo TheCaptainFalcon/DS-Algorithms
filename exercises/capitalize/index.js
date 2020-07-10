e40231de1974c010916d79b15bbfb0445841d3ed
Joseph <josephkim570@gmail.com>
2020-07-09 20:48:44 -0400

    Added solution for Capitalize with an example code to test out behaviorials of indexing and slice. The biggest key here was the extra space in split.

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,22 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+
+    // to understand behavior between indexing and slice
+    let example = 'hello there sir'
+
+    example[0]
+    example.slice(1)
+
+    let arr = [];
+    
+    // key is the extra space in split to separate by words into a separate array
+    for(let word of str.split(' ')) {
+    arr.push(word[0].toUpperCase() + word.slice(1))
+    }
+  
+  return arr.join(' ')
+}
 
 module.exports = capitalize;
