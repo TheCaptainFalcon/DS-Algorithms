9817e5706979d45c274a0be1bf4485c2b1f60d60
Joseph <josephkim570@gmail.com>
2020-07-14 17:52:19 -0400

    Added solutions for pyramid, note that this is the step-up/upgrade from steps exercise.

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,21 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    // needs double parenthesis for expression rules
+    let midpoint = Math.floor((2 * n - 1) / 2)
+    for(let row = 0; row < n; row++) {
+        let stair = '';
+        for(let column = 0; column < 2 * n - 1; column++) {
+            // creates an 'in between' barrier, therefore must be = to column value
+            if(midpoint - row <= column && midpoint + row >= column) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            }
+        }
+        console.log(stair)
+    }
+}
 
 module.exports = pyramid;
