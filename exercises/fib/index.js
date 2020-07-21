5cc80af429a8463400ee45c2ba2fcdd26ce2f7a4
Joseph <josephkim570@gmail.com>
2020-07-20 21:39:23 -0400

    Improved recursive solution in fibonacci (exponential runtime) using memoization.

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,26 +8,49 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-    // because the first two numbers will always be constant and the fact that there are no previous 2 numbers for those entries
-    let result = [0, 1] 
+// // iterative solution - note that this is a linear runtime ex
+
+// function fib(n) {
+//     // because the first two numbers will always be constant and the fact that there are no previous 2 numbers for those entries
+//     let result = [0, 1] 
  
-    for(let i = 2; i <= n; i++) {
-        let a = result[i-1];
-     let b = result[i-2];
-      let c = a + b;
-      result.push(c)
+//     for(let i = 2; i <= n; i++) {
+//         let a = result[i-1];
+//      let b = result[i-2];
+//       let c = a + b;
+//       result.push(c)
+//     }
+//      return result[n]
+// }
+
+// recursive solution
+
+function slowFib(n) {
+    if (n < 2) {
+        return n;
+    } else {
+    return fib(n-2) + fib(n-1);
     }
-     return result[n]
 }
 
-// // recursion solution
-// function fib(n) {
-//     if (n < 2) {
-//         return n;
-//     } else {
-//     return fib(n-2) + fib(n-1);
-//     }
-// }
+// memoized (improved) solution of the recursive solution
+// utilizes slowFib above
+const fib = memoize(slowFib);
+
+function memoize(fn) {
+    let cache = {};
+
+    return function(...args) {
+        if(cache[args]) {
+            return cache[args]
+        } else {
+        // whenever we call a function with an array of arguments, we must use the apply helper function
+        let result = fn.apply(this, args)
+        cache[args] = result;
+
+        return result;
+        }
+    }
+}
 
 module.exports = fib;
