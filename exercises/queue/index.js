6064b6c66f922876a5c5af891c8be7b675909e72
Joseph <josephkim570@gmail.com>
2020-07-22 23:17:57 -0400

    Added solution for the Queue data structure tutorial problem.

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,21 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+    // For a constructor, when we create a new instance of a class, the constructor will automatically be called when creating a new instance
+    constructor() {
+        this.data = [];
+    }
+
+    add(record) {
+        // unshift to push into front of array, as opposed to end with push
+        this.data.unshift(record)
+    }
+
+    remove() {
+        // remove whatever is at the end - but also return to be able to work with what we removed
+        return this.data.pop();
+    }
+}
 
 module.exports = Queue;
