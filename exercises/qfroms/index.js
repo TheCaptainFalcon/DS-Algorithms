12bbb53f2ddba6b8cc15d9e1e98fd94eead26e69
Joseph <josephkim570@gmail.com>
2020-07-24 02:13:08 -0400

    Added solution for Queue from Stack (qfroms).

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,42 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.first = new Stack();
+        this.second = new Stack();
+    }
+
+    add(record) {
+        this.first.push(record);
+    }
+
+    remove() {
+        // moves all stack A records to stack B
+        while (this.first.peek()) {
+            this.second.push(this.first.pop());
+        }
+        let record = this.second.pop()
+
+        while(this.second.peek()) {
+            this.first.push(this.second.pop());
+        }
+        return record;
+    }
+
+    peek() {
+        while(this.first.peek()) {
+            this.second.push(this.first.pop());
+        }
+
+        let record = this.second.peek();
+        
+        while(this.second.peek()) {
+            this.first.push(this.second.pop())
+        }
+        return record;
+    }
+    
+}
 
 module.exports = Queue;
