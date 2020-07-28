b9278a5d9efdc036b99baf540674d9a747a67067
Joseph <josephkim570@gmail.com>
2020-07-27 20:43:07 -0400

    Added some solutions to the linkedList ds there are code pertaining to the Node and LinkedList classes, alongside the methods including insertFirst, size, getFirst, getLast, clear, removeLast, insertLast, getAt.

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,123 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+    }
+
+    size() {
+        let counter = 0;
+        let node = this.head;
+
+        // while node is a truthy value (null is falsey, thus tails do not continue()
+        while(node) {
+            counter++;
+            node = node.next;
+        }
+        return counter;
+    }
+    
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        // edge-case to see if there are no nodes
+        if (!this.head) {
+            return null;
+        }
+
+        let node = this.head
+        while(node) {
+            if(!node.next) {
+                return node;
+            }
+            node = node.next;
+        }
+    }
+
+    clear() {
+        // although there may be nodes in memory, as far as the linked list is concerned, there are no nodes
+        this.head = null;
+    }
+
+    removeFirst() {
+        if(!this.head) {
+            return null;
+        }
+
+        let node = this.head
+        this.head = node.next
+        // or can do this.head = this.head.next (per solution vid)
+        
+    }
+
+    removeLast() {
+        // edge case for no nodes
+        if(!this.head) {
+            return null;
+        }
+
+        // edge case for 1 node
+        if(!this.head.next) {
+            return this.head = null;
+        }
+
+        let previous = this.head
+        let node = this.head.next
+        while(node.next) {
+            previous = node
+            node = node.next
+        }
+        previous.next = null;
+    }
+
+    insertLast(data) {
+        let last = this.getLast();
+
+        if(last) {
+            // if there are nodes in the linked list
+            last.next = new Node(data)
+        } else {
+            // if there are no existing nodes
+            this.head = new Node(data)
+        }
+    }
+
+    getAt(index) {
+        // edge case for no nodes
+        // also may not be necessary because of while loop exit at bottom
+        if(!this.head) {
+            return null;
+        }
+
+        let node = this.head;
+        let counter = 0;
+
+        while(node) {
+            if(counter === index) {
+                return node;
+            } 
+            counter++;
+            node = node.next;
+            }
+            // edge case for index out of bounds
+            // unable to find correct node - exit while loop 
+            return null;
+        }
+    }
+
 
 module.exports = { Node, LinkedList };
