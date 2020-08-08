92dc269ab95aff7c6d9f512a48368d743aa88c84
Joseph <josephkim570@gmail.com>
2020-08-07 23:28:22 -0400

    Added levelWidth solution, which is a manual built method based on the breadth traversal method in a tree.

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,25 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    // peculiar how the stopper is included in the initial starting arr
+    let arr = [root, 's'];
+    let counter = [0];
+
+    while(arr.length > 1) {
+        // although i thought this was initially just for the 's', but this logic 
+        // applies for every element as normal logic, but it is still a key for both
+        let node = arr.shift()
+
+        if(node === 's') {
+            counter.push(0)
+            arr.push('s')
+        } else {
+            arr.push(...node.children)
+            counter[counter.length -1]++;
+        }
+    }
+    return counter;
+}
 
 module.exports = levelWidth;
